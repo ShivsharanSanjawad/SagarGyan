c31d22595ccd73a911988322c7a346b65dd039d9
vedaant <vedaant.mahale@gmail.com>
2025-09-21 13:49:26 +0530

    elastic search working

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,25 @@
 import express from "express";
 import cors from "cors";
 import ingestDWCA from "./routes/ingestdwca.js";
+import elasticSearch from "./routes/elasticsearch.js";
+
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: "http://localhost:5173", // allow your frontend
 }));
 
-app.use("/api", ingestDWCA);
+app.use(express.json()); // parse JSON bodies
 
+// API routes
+app.use("/api", ingestDWCA);
+app.use("/api", elasticSearch);
+
+// Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
